cba278cb8335000274475b920862c71ebaf97d91
Sebastian Jensen <111466887+HEXtriple@users.noreply.github.com>
2024-03-13 13:03:26 +0100

    reform av projekt, forum istället för spel

---
+++ js/cultButton.js
@@ -0,0 +1,7 @@
+let heroImage = document.getElementsByClassName(".hero-image");
+
+function cultJoiner(){
+    heroImage.style.backgroundImage = "url('../Images/Danderyd.jpg')";
+    alert("You have joined the cult");
+}
+
